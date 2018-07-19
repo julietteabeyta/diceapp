935bc548a3d4360b64e1c87236a6d3bd02cf6e34
julietteabeyta <juneauwh0a@gmail.com>
2018-07-18 22:55:45 -0600

    Add styling to dice values::WIP making it responsive 😢

--- src/Die.js
+++ src/Die.js
@@ -19,15 +19,16 @@ class Die extends React.Component {
         e.preventDefault();
 
         const sides = this.props.sides;
-        this.setState({rollValue: Math.floor(Math.random() * sides), rolling: true});
+        this.setState({rollValue: Math.ceil(Math.random() * sides), rolling: true});
     }
     render() {
         const sides = this.props.sides;
         const sidesString = `d${ sides }`;
+        const isD4 = sides === 4;
         return (
-            <div id={sidesString} className={ this.state.rolling ? 'rolling' : ''}  onClick={this.roll}>
+            <div id={sidesString} className={this.state.rolling ? 'rolling die' : 'die'} data-d4={isD4 ? this.state.rollValue : null }  onClick={this.roll}>
                 <div className="readout">{this.state.rollValue}</div>
-                <ReactSVG svgClassName='die-svg' path={require(`./svg/${sidesString}.svg`)} />
+                <ReactSVG svgClassName='die-svg'path={require(`./svg/${sidesString}.svg`)} />
             </div>
         )
     }
